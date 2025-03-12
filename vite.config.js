796f01f7da128c6559c9fe96c76f43149eff8749
oumar782 <oumardiane399@gmail.com>
2025-03-12 12:21:05 +0000

    mise en forme du heros

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    // Diviser les dépendances en chunks séparés
+                    react: ['react', 'react-dom'],
+                    reactRouter: ['react-router-dom'],
+                    // Ajoutez d'autres dépendances ici
+                },
+            },
+        },
+        chunkSizeWarningLimit: 1000, // Augmenter la limite d'avertissement si nécessaire
+    },
+});
