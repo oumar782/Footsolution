63949051edecc4f08a20d96df815b287232f2c8f
oumar782 <oumardiane399@gmail.com>
2025-03-15 14:33:42 +0000

    mise en fou

--- src/composant/imageslider.jsx
+++ src/composant/imageslider.jsx
@@ -42,39 +42,44 @@ const ImageSlider = () => {
   }, [text, isDeleting, loopNum, texts]);
 
   return (
-    <section className="homepage" id="home">
-      <div className="video-background">
-        <video autoPlay muted loop playsInline aria-label="Vidéo de promotion de Foot Space Réservé">
-          <source src={videoFile} type="video/mp4" />
-          Votre navigateur ne supporte pas la vidéo HTML5.
-        </video>
+    <div className="homepage" id="home">
+    {/* Vidéo de fond */}
+    <div className="video-background">
+      <video autoPlay muted loop playsInline aria-label="Vidéo de promotion de Foot Space Réservé">
+        <source src={videoFile} type="video/mp4" />
+        Votre navigateur ne supporte pas la vidéo HTML5.
+      </video>
+      {/* Calque flouté en vert */}
+      <div className="video-overlay"></div>
+    </div>
+  
+    {/* Contenu principal */}
+    <div className="content">
+      <div className="textss">
+        <h1>
+          <span className="typewriter">{text}</span>
+          <span className="cursor">|</span>
+        </h1>
+        <p>
+          Révolutionnez la gestion de vos terrains de football avec FootSpace Solutions. Simplifiez la réservation, optimisez vos créneaux et maximisez vos revenus, tout en offrant à vos clients une expérience moderne et fluide.
+        </p>
       </div>
-      <div className="content">
-        <div className="textss">
-          <h1>
-            <span className="typewriter">{text}</span>
-            <span className="cursor">|</span>
-          </h1>
-          <p>
-          Révolutionnez la gestion de vos terrains de football avec FootSpace Solutions. Simplifiez la réservation, optimisez vos créneaux et maximisez vos revenus, tout en offrant à vos clients une expérience moderne et fluide
-          </p>
-        </div>
-        <div className="ok">
-          <a
-            style={{ backgroundColor: 'rgba(0, 128, 0, 0.5)', backdropFilter: 'blur(10px)' }}
-            href="/contact"
-          >
-            Demander une démo →
-          </a>
-          <a
-            style={{ backgroundColor: 'rgba(8, 8, 8, 0.5)', backdropFilter: 'blur(10px)' }}
-            href="/features"
-          >
-            Découvrir les fonctionnalités
-          </a>
-        </div>
+      <div className="ok">
+        <a
+          style={{ backgroundColor: 'rgba(0, 128, 0, 0.5)', backdropFilter: 'blur(10px)' }}
+          href="/contact"
+        >
+          Demander une démo →
+        </a>
+        <a
+          style={{ backgroundColor: 'rgba(8, 8, 8, 0.5)', backdropFilter: 'blur(10px)' }}
+          href="/features"
+        >
+          Découvrir les fonctionnalités
+        </a>
       </div>
-    </section>
+    </div>
+  </div>
   );
 };
 
