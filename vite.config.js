320b6444e4b92b9f4457307002fa7fe3c0b7fa97
oumar782 <oumardiane399@gmail.com>
2025-03-29 16:32:54 +0000

    Forcer la mise à jour sur GitHb

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    build: {
-        sourcemap: true, // Activer les sourcemaps
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    // Diviser les dépendances en chunks séparés
-                    react: ['react', 'react-dom'],
-                    radix: ['@radix-ui/react-tabs'],
-                },
-            },
-        },
-        chunkSizeWarningLimit: 1000, // Augmenter la limite d'avertissement si nécessaire
+  plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
     },
-});
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return
+        }
+        warn(warning)
+      }
+    }
+  }
+})
